22d0519d8876115b63481e34f363c5b3d989341b
Pushpalatha77 <121229708+Pushpalatha77@users.noreply.github.com>
2023-02-18 14:13:24 +0530

    login

--- flightbooking/src/components/flight-components/addFlight.js
+++ flightbooking/src/components/flight-components/addFlight.js
@@ -13,6 +13,7 @@ export class AddFlight extends Component {
                 arrivalCity: '',
                 departureCity: '',
                 departureDate: '',
+                
             },
             errors: {},
             msg: '',

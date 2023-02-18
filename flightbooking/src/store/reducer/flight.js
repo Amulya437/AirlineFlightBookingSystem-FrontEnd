dddf782cb910a7dce447b4a41a928c039c4bcc92
Pushpalatha77 <121229708+Pushpalatha77@users.noreply.github.com>
2023-02-18 10:08:23 +0530

    Flight added

--- flightbooking/src/store/reducer/flight.js
+++ flightbooking/src/store/reducer/flight.js
@@ -1,9 +1,16 @@
 const initialState = {
     list: []
-}; const flight = (state = initialState, action) => {
+}; 
+const flight = (state = initialState, action) => {
     if (action.type === 'GET_LIST_FLIGHT') {
         return { ...state, list: action.payload }
     }
-    return state;
-};
-export default flight;
+    //payload object(employee) will get added in list   
+    if (action.type === 'ADD_FLIGHT') {
+        return {
+            ...state,
+            list: [...state.list, action.payload]}
+        }
+        return state;
+    };
+    export default flight;
